b4872ba4b4d1ba6654eb795c297915f1917e129f
Evaldo Santos <jesantosjr@protonmail.com>
2021-05-14 17:17:21 -0300

    navigating to hero details delete hero details from HeroesComponent add a hero detail route DashboardComponent hero links HeroesComponent hero links remove deead code Routable HeroDetailComponent Extract the id route parameter add HeroService.getHero()

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,24 +9,12 @@ import { MessageService } from '../message.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
-
   heroes: Hero[] = [];
 
-  selectedHero?: Hero;
-
-  constructor(private heroService: HeroService, private messageService: MessageService) { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit(): void {
     this.getHeroes();
-  }
-
-  onSelect(hero: Hero) {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`)
   }
 
   getHeroes(): void {
